029cef8421bc35be20ae46d65ebbd264333f6b69
BuildTools <olmorris@ucalgary.ca>
2021-03-30 18:11:21 -0600

    New Nub Script
    Removed extraneous click and drag script and replaced with nub script to make lines between chatnodes.

--- Assets/ConnectionNub.cs
+++ Assets/ConnectionNub.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickAndDragNode : MonoBehaviour {
+public class ConnectionNub : MonoBehaviour {
+
+    public ConnectionNub connectedNub;
+    private SpriteRenderer spriteRenderer;
+    private LineRenderer line;
 
     private bool mouseHeldDown;
     private float clickPosX;
@@ -11,14 +15,19 @@ public class ClickAndDragNode : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         mouseHeldDown = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        line = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update() {
         if (mouseHeldDown) {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            gameObject.transform.localPosition = new Vector3(mousePos.x - clickPosX, mousePos.y - clickPosY, transform.localPosition.z);
+            //gameObject.transform.localPosition = new Vector3(mousePos.x - clickPosX, mousePos.y - clickPosY, transform.localPosition.z);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, mousePos);
         }
+        spriteRenderer.color = connectedNub == null ? Color.red : Color.cyan;
     }
 
     private void OnMouseDown() {
@@ -32,5 +41,8 @@ public class ClickAndDragNode : MonoBehaviour {
 
     private void OnMouseUp() {
         mouseHeldDown = false;
+
+        //TODO check if another nub is below the mouse and connect it to this nub
     }
+
 }
