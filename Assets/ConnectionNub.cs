c78cd24ad44e259eea66f251b2b91ca6b8401130
BuildTools <olmorris@ucalgary.ca>
2021-03-31 16:32:44 -0600

    Nub Connection Now Working
    "Nubs" can be connected to each other through code and visually via a line renderer. This is done by clicking and dragging between them, which doubly-links them. Clicking on either node will undo the link on both ends.

--- Assets/ConnectionNub.cs
+++ Assets/ConnectionNub.cs
@@ -23,7 +23,6 @@ public class ConnectionNub : MonoBehaviour {
     void Update() {
         if (mouseHeldDown) {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //gameObject.transform.localPosition = new Vector3(mousePos.x - clickPosX, mousePos.y - clickPosY, transform.localPosition.z);
             line.SetPosition(0, transform.position);
             line.SetPosition(1, mousePos);
         }
@@ -36,13 +35,46 @@ public class ConnectionNub : MonoBehaviour {
             clickPosX = mousePos.x - transform.localPosition.x;
             clickPosY = mousePos.y - transform.localPosition.y;
             mouseHeldDown = true;
+
+            //decouple nubs if one is clicked, and reset their line
+            if(connectedNub != null) {
+                connectedNub.connectedNub = null;
+                connectedNub.gameObject.GetComponent<LineRenderer>().SetPosition(1, connectedNub.gameObject.transform.position);
+            }
+            connectedNub = null;
         }
     }
 
     private void OnMouseUp() {
+        Debug.Log("let go");
+
         mouseHeldDown = false;
 
-        //TODO check if another nub is below the mouse and connect it to this nub
+        //check if there is a nub below the mouse when it is released
+        bool nubBelow = false;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D ray = Physics2D.Raycast(mousePos, new Vector3(0,0,1), 10);
+        if(ray.collider != null) {
+            if (ray.collider.gameObject.CompareTag("Nub") && ray.collider.gameObject != this.gameObject) { //check if the collided obj is a non-self nub
+                nubBelow = true;
+            }
+        }
+
+        if(!nubBelow) {
+            line.SetPosition(1, transform.position); //reset, no nub
+        }
+        else {
+            Debug.Log("CONNECTION: " + ray.collider.gameObject.name);
+
+            //connect to the other nub
+            GameObject connectingTo = ray.collider.gameObject;
+            connectedNub = connectingTo.GetComponent<ConnectionNub>();
+            line.SetPosition(1, connectingTo.transform.position);
+
+            //make sure they are doubly linked
+            connectedNub.connectedNub = this;
+        }
     }
 
 }
