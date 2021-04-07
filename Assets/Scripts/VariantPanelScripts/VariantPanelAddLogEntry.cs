db132344bbef666388c7df629063d51f3a972d4a
BuildTools <olmorris@ucalgary.ca>
2021-04-06 18:16:41 -0600

    Implemented Two More NodeTypes
    AddFeedArticle and AddLogEntry nodes implemented.

---
+++ Assets/Scripts/VariantPanelScripts/VariantPanelAddLogEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariantPanelAddLogEntry : VariantPanel {
+
+    public TMPro.TMP_InputField logTextInputField;
+    public ConnectionNub nextNub;
+
+    public override Dictionary<string, string> GetVariantPanelData() {
+        Dictionary<string, string> ret = new Dictionary<string, string>();
+        ret.Add("param", logTextInputField.text);   //TODO post processing on the dialogue for quotes and segments and such, can be done when serializing to JSON
+
+        ConnectionNub nubOnNextNode = nextNub.connectedNub;
+        ret.Add("next", nubOnNextNode == null ? "TERMINATE" : nubOnNextNode.GetParentChatNode().GetID());
+
+        return ret;
+    }
+
+    public override void PopulateVariantPanelData(Dictionary<string, string> savedData) {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<ChatNode> GetDescendantChatNodes() {
+
+        if (nextNub.connectedNub == null) {
+            return new List<ChatNode> { }; //no connection => no descendants
+        }
+        else {
+            return new List<ChatNode> { nextNub.connectedNub.GetParentChatNode() };
+        }
+
+    }
+}
