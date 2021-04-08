b3cf0a83e6393e7535f8aa54f3c24898b00da5a5
BuildTools <olmorris@ucalgary.ca>
2021-04-07 20:39:22 -0600

    Implemented NewSpeaker Node
    Implemented the NewSpeaker ChatNode.

---
+++ Assets/Scripts/VariantPanelScripts/VariantPanelNewSpeaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariantPanelNewSpeaker : VariantPanel {
+
+    public TMPro.TMP_InputField newSpeakerNameInputField;
+    public TMPro.TMP_InputField newSpeakerBNETIDInputField;
+    public ConnectionNub nextNub;
+
+    public override Dictionary<string, string> GetVariantPanelData() {
+
+        Dictionary<string, string> ret = new Dictionary<string, string>();
+        ret.Add("param", newSpeakerNameInputField.text);
+        ret.Add("bnetid", newSpeakerBNETIDInputField.text);
+
+        ConnectionNub nubOnNextNode = nextNub.connectedNub;
+        ret.Add("next", nubOnNextNode == null ? "TERMINATE" : nubOnNextNode.GetParentChatNode().GetID());
+
+        return ret;
+    }
+
+    public override void PopulateVariantPanelData(Dictionary<string, string> savedData) {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<ChatNode> GetDescendantChatNodes() {
+
+        if (nextNub.connectedNub == null) {
+            return new List<ChatNode> { }; //no connection => no descendants
+        }
+        else {
+            return new List<ChatNode> { nextNub.connectedNub.GetParentChatNode() };
+        }
+    }
+}
