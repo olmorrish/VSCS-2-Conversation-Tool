7370e254effbb09acced121987e5ded8e653488c
BuildTools <olmorris@ucalgary.ca>
2021-04-08 19:16:09 -0600

    Implemented StoreBool Node
    Implemented the StoreBool ChatNode.

---
+++ Assets/Scripts/VariantPanelScripts/VariantPanelStoreBool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VariantPanelStoreBool : VariantPanel {
+
+    public TMPro.TMP_InputField boolNameInputField;
+    public Toggle toggle;
+    public ConnectionNub nextNub;
+
+    public override Dictionary<string, string> GetVariantPanelData() {
+
+        Dictionary<string, string> ret = new Dictionary<string, string>();
+        ret.Add("param", boolNameInputField.text);
+
+        ret.Add("boolparam", toggle.isOn ? "true" : "false");
+
+        ConnectionNub nubOnNextNode = nextNub.connectedNub;
+        ret.Add("next", nubOnNextNode == null ? "TERMINATE" : nubOnNextNode.GetParentChatNode().GetID());
+
+        return ret;
+    }
+
+    public override void PopulateVariantPanelData(Dictionary<string, string> savedData) {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<ChatNode> GetDescendantChatNodes() {
+
+        if (nextNub.connectedNub == null) {
+            return new List<ChatNode> { }; //no connection => no descendants
+        }
+        else {
+            return new List<ChatNode> { nextNub.connectedNub.GetParentChatNode() };
+        }
+    }
+
+}
