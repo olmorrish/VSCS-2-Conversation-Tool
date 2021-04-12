60780ff45ca5c620bdd3b4f7b7020e40c43ead51
BuildTools <olmorris@ucalgary.ca>
2021-04-12 11:21:41 -0600

    Implemented BranchOnPlayerInput Node
    Implemented the BranchOnPlayerInput ChatNode. Options B and C can be disabled via a toggle; this will auto-disconnect their next nodes and disable the text field for editing, and will also not export them for serialization.

---
+++ Assets/Scripts/VariantPanelScripts/VariantPanelBranchOnPlayerInput.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VariantPanelBranchOnPlayerInput : VariantPanel {
+
+    public TMPro.TMP_InputField optionAInputField;
+    public TMPro.TMP_InputField optionBInputField;
+    public TMPro.TMP_InputField optionCInputField;
+
+    public Toggle optionBToggle;
+    public Toggle optionCToggle;
+
+    public ConnectionNub nextANub;
+    public ConnectionNub nextBNub;
+    public ConnectionNub nextCNub;
+
+    private void Update() {
+
+        optionBInputField.enabled = optionBToggle.isOn;
+        optionCInputField.enabled = optionCToggle.isOn;
+
+        if (!optionBToggle.isOn) {
+            nextBNub.DisconnectNub();
+        }
+        if (!optionCToggle.isOn) {
+            nextCNub.DisconnectNub();
+        }
+    }
+
+    public override Dictionary<string, string> GetVariantPanelData() {
+
+        Dictionary<string, string> ret = new Dictionary<string, string>();
+
+        ret.Add("optionA", optionAInputField.text);
+        ConnectionNub nubOnNextNode = nextANub.connectedNub;
+        ret.Add("nextA", nubOnNextNode == null ? "TERMINATE" : nubOnNextNode.GetParentChatNode().GetID());
+
+        if (optionBToggle.isOn) {
+            ret.Add("optionB", optionBInputField.text);
+            nubOnNextNode = nextBNub.connectedNub;
+            ret.Add("nextB", nubOnNextNode == null ? "TERMINATE" : nubOnNextNode.GetParentChatNode().GetID());
+        }
+
+        if (optionCToggle.isOn) {
+            ret.Add("optionC", optionCInputField.text);
+            nubOnNextNode = nextCNub.connectedNub;
+            ret.Add("nextC", nubOnNextNode == null ? "TERMINATE" : nubOnNextNode.GetParentChatNode().GetID());
+        }
+
+        return ret;
+    }
+
+    public override void PopulateVariantPanelData(Dictionary<string, string> savedData) {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<ChatNode> GetDescendantChatNodes() {
+
+        List<ChatNode> nexts = new List<ChatNode>();
+
+        if (nextANub.connectedNub != null) { //either one of the connections may be null
+            nexts.Add(nextANub.connectedNub.GetParentChatNode());
+        }
+        if (nextBNub.connectedNub != null && optionBToggle.isOn) {
+            nexts.Add(nextBNub.connectedNub.GetParentChatNode());
+        }
+        if (nextCNub.connectedNub != null && optionCToggle.isOn) {
+            nexts.Add(nextCNub.connectedNub.GetParentChatNode());
+        }
+
+        return nexts;
+    }
+}
