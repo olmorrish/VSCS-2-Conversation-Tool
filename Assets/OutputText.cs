fe5a4c70fa967dd66e07de8310bb728a8d18ca09
BuildTools <olmorris@ucalgary.ca>
2021-04-22 16:38:30 -0600

    Improved Error/Success Message Feedback
    The feedback messages will now flash red on errors, green on successes, and yellow otherwise. Before this change it was sometimes hard to tell when a new message had arrived of what the result of an export was.

--- Assets/OutputText.cs
+++ Assets/OutputText.cs
@@ -6,13 +6,32 @@ using UnityEngine;
 public class OutputText : MonoBehaviour {
 
     public TextMeshProUGUI textMesh;
+    public float fadeSpeed;
 
     // Start is called before the first frame update
     void Start() {
         textMesh.text = ("Info and errors display here.");
     }
 
+    private void Update() {
+
+        float r = Mathf.Lerp(textMesh.color.r, Color.white.r, fadeSpeed);
+        float g = Mathf.Lerp(textMesh.color.g, Color.white.g, fadeSpeed);
+        float b = Mathf.Lerp(textMesh.color.b, Color.white.b, fadeSpeed);
+
+        textMesh.color = new Color(r, g, b);
+    }
+
+
     public void AddLine(string line) {
         textMesh.text = line + "\n" + textMesh.text;
+
+        if (line.Contains("ERROR"))
+            textMesh.color = Color.red;
+        else if (line.Contains("SUCCESS"))
+            textMesh.color = Color.green;
+        else
+            textMesh.color = Color.yellow;
+
     }
 }
