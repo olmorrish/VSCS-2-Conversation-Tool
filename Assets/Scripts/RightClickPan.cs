279f1459f1c6f6f73e915b0ace17c6433b7aa4ab
BuildTools <olmorris@ucalgary.ca>
2021-04-22 13:29:09 -0600

    Added Directional Zoom Movement
    If the mouse is a certain distance from the center of the screen, the camera will now translate proportionally to allow directional movement towards the pointer when zooming. This is inverted when zooming out, and there is an appropriate dead zone in the center.

--- Assets/Scripts/RightClickPan.cs
+++ Assets/Scripts/RightClickPan.cs
@@ -6,9 +6,12 @@ public class RightClickPan : MonoBehaviour {
 
     Vector3 mousePos;
     Vector3 mousePosDiff;
-    public float scrollSensitivity;
-    public float minZoom;
-    public float maxZoom;
+    public float scrollSensitivity = 15f;
+    public float minZoom = 10f;
+    public float maxZoom = 100f;
+
+    Vector3 mousePosOffsetFromCenter;
+    public float directionalScrollMultiplier;
 
     // Start is called before the first frame update
     void Start() {
@@ -20,8 +23,8 @@ public class RightClickPan : MonoBehaviour {
         //get mouse position delta
         mousePosDiff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mousePos;
 
-        //if rmb is held down, move the camera in the opposite direction to the mouse movement
-        if (Input.GetMouseButtonDown(1)) {
+        //if rmb or mmb are held down, move the camera in the opposite direction to the mouse movement
+        if (Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)) {
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
         else if (Input.GetMouseButton(1)) {
@@ -30,8 +33,23 @@ public class RightClickPan : MonoBehaviour {
 
         //zoom with scroll
         float fov = Camera.main.orthographicSize;
-        fov -= Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity;
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        fov -= scrollInput * scrollSensitivity;
         fov = Mathf.Clamp(fov, minZoom, maxZoom);
         Camera.main.orthographicSize = fov;
+
+        //displace the camera on zoom based on position
+        mousePosOffsetFromCenter = new Vector3(Input.mousePosition.x - Screen.width/2, Input.mousePosition.y - Screen.height/2, 0f);
+
+        if(mousePosOffsetFromCenter.magnitude > 100f && Mathf.Abs(scrollInput) > 0f) {
+
+            if (scrollInput > 0f) {
+                Camera.main.transform.localPosition = Camera.main.transform.localPosition + (mousePosOffsetFromCenter * (directionalScrollMultiplier));
+            }
+            else if(scrollInput < 0f) {
+                Camera.main.transform.localPosition = Camera.main.transform.localPosition + (mousePosOffsetFromCenter * -(directionalScrollMultiplier));
+            }
+        }
+
     }
 }
