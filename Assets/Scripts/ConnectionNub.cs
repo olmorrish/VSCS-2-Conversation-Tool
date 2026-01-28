af9ded545b0effe87f9f49feedbc6eed9b588eab
Oliver Morrish <olmorrish@gmail.com>
2026-01-27 22:28:51 -0700

    Clean up nub script

--- Assets/Scripts/ConnectionNub.cs
+++ Assets/Scripts/ConnectionNub.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ConnectionNub : MonoBehaviour {
 
@@ -13,6 +11,8 @@ public class ConnectionNub : MonoBehaviour {
     private float clickPosX;
     private float clickPosY;
 
+    private const string TAG_NUB = "Nub";
+
     // Start is called before the first frame update
     void Start() {
         mouseHeldDown = false;
@@ -24,7 +24,7 @@ public class ConnectionNub : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        //redraw the connection each frame if there is a connected node, in case one moves.
+        // redraw the connection each frame if there is a connected node, in case one moves.
         if(connectedNub != null) {
             line.SetPosition(0, transform.position);
             line.SetPosition(1, connectedNub.transform.position);
@@ -33,14 +33,14 @@ public class ConnectionNub : MonoBehaviour {
             ResetLineRenderer();
         }
 
-        //always draw a line from the clicked node to the mouse, while M0 is held down
+        // always draw a line from the clicked node to the mouse, while M0 is held down
         if (mouseHeldDown) {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             line.SetPosition(0, transform.position);
             line.SetPosition(1, mousePos);
         }
 
-        //nubs change colour to indicate connection
+        // nubs change colour to indicate connection
         spriteRenderer.color = connectedNub == null ? defaultColor : Color.cyan;
     }
 
@@ -54,36 +54,34 @@ public class ConnectionNub : MonoBehaviour {
             clickPosY = mousePos.y - transform.localPosition.y;
             mouseHeldDown = true;
 
-            //decouple nubs if one is clicked, and reset their line
+            // decouple nubs if one is clicked, and reset their line
             DisconnectNub();
         }
     }
 
     /// <summary>
-    /// Called on mosue 1 up; releasing over another nub creates a connection, otherwise the connection is intentionally broken.
+    /// Called on mouse 1 up; releasing over another nub creates a connection, otherwise the connection is intentionally broken.
     /// </summary>
     private void OnMouseUp() {
 
         mouseHeldDown = false;
 
-        //check if there is a nub below the mouse when it is released
+        // check if there is a nub below the mouse when it is released
         bool nubBelow = false;
 
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D ray = Physics2D.Raycast(mousePos, new Vector3(0,0,1), 10);
         if(ray.collider != null) {
-            if (ray.collider.gameObject.CompareTag("Nub") && ray.collider.gameObject != this.gameObject) { //check if the collided obj is a non-self nub
+            if (ray.collider.gameObject.CompareTag(TAG_NUB) && ray.collider.gameObject != this.gameObject) { //check if the collided obj is a non-self nub
                 nubBelow = true;
             }
         }
 
         if(!nubBelow) {
-            ResetLineRenderer(); //no nub
+            ResetLineRenderer(); // no nub
         }
         else {
             ConnectToNub(ray.collider.gameObject.GetComponent<ConnectionNub>());
-
-
         }
     }
 
@@ -93,15 +91,13 @@ public class ConnectionNub : MonoBehaviour {
     public void ConnectToNub(ConnectionNub otherNub) {
         connectedNub = otherNub;
 
-        //Disconnect other nub then doubly link to us
+        // disconnect other nub then doubly link to us
         connectedNub.DisconnectNub();
         connectedNub.connectedNub = this;
-
-        //line.SetPosition(1, connectedNub.gameObject.transform.position);
     }
 
     /// <summary>
-    /// Disconnects the nubs. Also tells other nub to reset.
+    /// Disconnects the nubs. Also tells other nub to disconnect.
     /// </summary>
     public void DisconnectNub() {
         if (connectedNub != null) {
@@ -112,15 +108,7 @@ public class ConnectionNub : MonoBehaviour {
         ResetLineRenderer();
     }
 
-    /// <summary>
-    /// Disconnects this nub only; this is required when this nub is on a copy of a node. Called on copyNode.
-    /// </summary>
-    public void DisconnectThisNubOnly() {
-        connectedNub = null;
-        ResetLineRenderer();
-    }
-
-    public void ResetLineRenderer() {
+    private void ResetLineRenderer() {
         if (line) {
             line.SetPosition(0, transform.position);
             line.SetPosition(1, transform.position);
@@ -128,12 +116,12 @@ public class ConnectionNub : MonoBehaviour {
     }
 
     /// <summary>
-    /// Recursively goes up the chain until the ChatNode this nub is attached to is found
+    /// Recursively goes up the chain until the ChatNode this nub is attached to is found.
     /// </summary>
     /// <returns></returns>
     public ChatNode GetParentChatNode() {
 
-        GameObject obj = this.gameObject;
+        GameObject obj = gameObject;
 
         while (obj.GetComponent<ChatNode>() == null) {
             obj = obj.transform.parent.gameObject;
