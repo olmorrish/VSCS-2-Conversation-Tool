21357f723ac0e1af236b2ff384beed422704284e
Oliver Morrish <olmorrish@gmail.com>
2026-01-29 00:14:50 -0700

    Improve right click pan runtime efficiency

--- Assets/Scripts/RightClickPan.cs
+++ Assets/Scripts/RightClickPan.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RightClickPan : MonoBehaviour {
 
@@ -12,42 +10,44 @@ public class RightClickPan : MonoBehaviour {
 
     Vector3 mousePosOffsetFromCenter;
     public float directionalScrollMultiplier;
+    private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start() {
+        mainCamera = Camera.main;
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Update is called once per frame
     void Update() {
-        //get mouse position delta
-        mousePosDiff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mousePos;
+        // get mouse position delta
+        mousePosDiff = mainCamera.ScreenToWorldPoint(Input.mousePosition) - mousePos;
 
-        //if rmb or mmb are held down, move the camera in the opposite direction to the mouse movement
+        // if RMB or MMB are held down, move the camera in the opposite direction to the mouse movement
         if (Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)) {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         }
         else if (Input.GetMouseButton(1)) {
             transform.position -= mousePosDiff;
         }
 
-        //zoom with scroll
-        float fov = Camera.main.orthographicSize;
+        // zoom with scroll
+        float fov = mainCamera.orthographicSize;
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
         fov -= scrollInput * scrollSensitivity;
         fov = Mathf.Clamp(fov, minZoom, maxZoom);
-        Camera.main.orthographicSize = fov;
+        mainCamera.orthographicSize = fov;
 
-        //displace the camera on zoom based on position
+        // displace the camera on zoom based on position
         mousePosOffsetFromCenter = new Vector3(Input.mousePosition.x - Screen.width/2, Input.mousePosition.y - Screen.height/2, 0f);
 
         if(mousePosOffsetFromCenter.magnitude > 100f && Mathf.Abs(scrollInput) > 0f) {
 
             if (scrollInput > 0f) {
-                Camera.main.transform.localPosition = Camera.main.transform.localPosition + (mousePosOffsetFromCenter * (directionalScrollMultiplier));
+                mainCamera.transform.localPosition += (mousePosOffsetFromCenter * (directionalScrollMultiplier));
             }
             else if(scrollInput < 0f) {
-                Camera.main.transform.localPosition = Camera.main.transform.localPosition + (mousePosOffsetFromCenter * -(directionalScrollMultiplier));
+                mainCamera.transform.localPosition += (mousePosOffsetFromCenter * -(directionalScrollMultiplier));
             }
         }
 
